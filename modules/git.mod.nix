6905537cf5554e1c1d984b996c1035b858858a13
Dylan Cleary <dylan@dylanc.com>
2026-07-04 11:09:04 -0400

    modules: group desktop, networking, and shell aspects into folders; drop zoxide

--- modules/git.mod.nix
+++ modules/git.mod.nix
@@ -9,8 +9,8 @@
         enable = true;
 
         settings = {
-          user.name = "Dylan";
-          user.email = "dylan@cleary.org";
+          user.name = "Dylan Cleary";
+          user.email = "dylan@dylanc.com";
 
           init.defaultBranch = "main";
           pull.rebase = true;
